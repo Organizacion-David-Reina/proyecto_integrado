91ee2dc72d8b6460ce61794428d61f9d58c5c71d
David Reina Cabrera <davidreinac.sanjosemlg@fundacionloyola.net>
2025-04-21 20:44:24 +0200

    Back del proyecto

---
+++ reina_cabrera_david/src/test/java/com/proyectointegrado/reina_cabrera_david/ReinaCabreraDavidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectointegrado.reina_cabrera_david;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReinaCabreraDavidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
